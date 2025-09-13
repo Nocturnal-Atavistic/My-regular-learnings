416a9ba1cd17945bf84f708e6760849533874818
Nocturnal-Atavistic <82271517+Nocturnal-Atavistic@users.noreply.github.com>
2025-09-13 16:44:37 +0400

    Create Star Pattern.py
    Learnign more about for loop through printing  star patterns

---
+++ Star Pattern.py
@@ -0,0 +1,20 @@
+for i in range(5):
+    print("*", end="\n")
+    for j in range(i)  :# in range (5):
+        # if i <5 and  j < 5:
+        print("*", end="")
+print("\n-------------------------------------")
+
+for i in range(5):
+    print("*", end="")
+    for j in range(5):
+        print("*", end="")
+print("\n-------------------------------------")
+
+
+for i in range(1,5):
+    print("*", end="")
+    print("", end="\n")
+    
+    for j in range(i):
+        print("*",end="" )   
