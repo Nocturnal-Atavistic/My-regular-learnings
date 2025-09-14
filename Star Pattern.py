566f860446f1177433bbd163476774e7f13dc922
Nocturnal-Atavistic <82271517+Nocturnal-Atavistic@users.noreply.github.com>
2025-09-14 17:43:20 +0400

    Update Star Pattern.py

--- Star Pattern.py
+++ Star Pattern.py
@@ -18,3 +18,23 @@ for i in range(1,5):
     
     for j in range(i):
         print("*",end="" )   
+
+# Inverted right triangle
+n = 5
+for i in range(n):
+    print("")
+    for j in range (n-i):
+        print("", end="")
+    print("*", end="")
+    for k in range(j):
+        print ("*", end="")
+
+# Right Triangle:
+print("\n-------------------------------------")
+for i in range (n-1):
+    print("*")
+    for j in range (i+1):
+        print("*", end="")
+        for k in range(j-1):
+            print("*", end="")
+
