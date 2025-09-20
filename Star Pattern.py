ba688cbed53d65fcacfd6644fb108859b0cb8ad4
Nocturnal-Atavistic <82271517+Nocturnal-Atavistic@users.noreply.github.com>
2025-09-20 17:57:47 +0400

    Update Star Pattern.py

--- Star Pattern.py
+++ Star Pattern.py
@@ -138,4 +138,17 @@ for i in range(n):
 
     print()
 
+print("\n-------------------------------------")
 
+# Reverse Pyramid
+print("14")
+n=5
+for i in range(n):
+    for j in range(i+1):
+        print(" ", end="")
+    for k in range(i,n-1):
+        print("*", end="")
+    for p in range(i,n):
+        print("*", end="")
+
+    print()
